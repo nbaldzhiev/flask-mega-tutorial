f8804a57ef122cb03da1aff5c3b0f236f2a24f30
nbaldzhi <nbaldzhi@cisco.com>
2020-04-16 18:26:21 +0300

    chapter 7 intermediate commit

--- app/forms.py
+++ app/forms.py
@@ -44,7 +44,8 @@ class EditProfileForm(FlaskForm):
                             [DataRequired(),Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
-    def __init__(self, original_username, *args, **kwargs):
+    def __init__(self, original_username):
+        super().__init__()
         self.original_username = original_username
 
     def validate_username(self, username):
--- app/models.py
+++ app/models.py
@@ -5,6 +5,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     """
     """
@@ -12,9 +19,16 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -28,6 +42,24 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query. \
+            join(followers, (followers.c.followed_id == Post.user_id)). \
+            filter(followers.c.follower_id == self.id). \
+            order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
