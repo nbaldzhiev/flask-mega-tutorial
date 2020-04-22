668a470cd712fe3a98033e946a01b274af2d29d3
nbaldzhi <nbaldzhi@cisco.com>
2020-04-22 18:59:16 +0300

    end of chapter 9

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config():
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dummynenko@gmail.com']
+    POSTS_PER_PAGE = 3
 
